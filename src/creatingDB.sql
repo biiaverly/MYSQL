13efc63c0ffad0a31349d0fae8845a31ae771724
Bia Verly <biaverly@bia.verly@oliveiratrust.com.br>
2023-08-03 12:21:33 -0300

    ManagingDB

--- src/creatingDB.sql
+++
@@ -1,8 +0,0 @@
--- Check if the database exists before creating it
-CREATE DATABASE IF NOT EXISTS Bia_test;
-
--- Use the database created or already exists
-USE Bia_test;
-
--- Show all databases to verify the creation
-SHOW DATABASES;
